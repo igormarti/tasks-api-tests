0db4cdb74e70ee8ea2ac6495a46e48dc9c9f82be
Igor Martins <igorm573@gmail.com>
2022-05-05 11:52:14 -0300

    init api tests

---
+++ src/main/java/br/im/imrocha/tasks/apitest/dto/Task.java
@@ -0,0 +1,10 @@
+package br.im.imrocha.tasks.apitest.dto;
+
+import java.time.LocalDate;
+
+public class Task {
+
+	public String task;
+	public String dueDate;
+	
+}
---
+++ src/test/java/br/im/imrocha/tasks/apitest/ApiTest.java
@@ -0,0 +1,72 @@
+package br.im.imrocha.tasks.apitest;
+
+import java.time.LocalDate;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.google.gson.Gson;
+
+import br.im.imrocha.tasks.apitest.dto.Task;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class ApiTest {
+
+	public static String objectToJSON(Object obj) {
+		return new Gson().toJson(obj);
+	}
+	
+	@BeforeClass
+	public static void setupTest() {
+		RestAssured.baseURI = "http://localhost:8081/tasks-backend";
+	}
+	
+	@Test
+	public void shouldReturnAllTasks() {
+		RestAssured
+		.given()
+		.when()
+			.get("/todo")
+		.then()
+			.statusCode(200);		
+	}
+	
+	@Test
+	public void shouldAddTask() {
+		
+		Task todo = new Task();
+		todo.task = "tests task";
+		todo.dueDate = LocalDate.now().plusMonths(1).toString();
+		
+		RestAssured
+		.given()
+			.body(objectToJSON(todo))
+			.contentType(ContentType.JSON)
+		.when()
+			.post("/todo")
+		.then()
+			.statusCode(201);		
+	}
+	
+	@Test
+	public void shouldNotAddInvalidTask() {
+		
+		Gson gson = new Gson();
+		Task todo = new Task();
+		todo.task = "tests task";
+		todo.dueDate = LocalDate.now().minusYears(10).toString();
+		
+		RestAssured
+		.given()
+			.body(gson.toJson(todo))
+			.contentType(ContentType.JSON)
+		.when()
+			.post("/todo")
+		.then()
+			.body("message", CoreMatchers.is("Due date must not be in past"))
+			.statusCode(400);		
+	}
+}
+
